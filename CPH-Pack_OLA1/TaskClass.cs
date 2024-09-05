3ad2269652ca60ed1f3ca5a13eef4684e0679c3c
albiek1 <54135401+albiek1@users.noreply.github.com>
2024-09-05 12:02:38 +0200

    Most of the program
    + TaskClass
    + Add Task
    + Add Task Group
    + Show All Tasks
    + Show All Groups
    Delete tasks doesn't remove tasks for testing purposes
    Whole thing runs on a state machine i learnt how to make years ago in processing. Entire program is held together with hopes and dreams
    
    For final commit, remove any emoticons or uwu speach

---
+++ CPH-Pack_OLA1/TaskClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CPH_Pack_OLA1
+{
+    internal class TaskClass
+    {
+        UniqueId TaskID;
+        String TaskName = "";
+        String TaskValue = "";
+        Boolean Completed = false;
+        public TaskClass(string name, string value)
+        {
+            this.TaskID = new UniqueId();
+            this.TaskName = name;
+            this.TaskValue = value;
+        }
+
+        public UniqueId GetID()
+        {
+            return this.TaskID;
+        }
+
+        public String GetTaskName()
+        {
+            return this.TaskName;
+        }
+
+        public void SetTaskName(String taskName)
+        {
+            this.TaskName = taskName;
+        }
+
+        public String GetTaskValue()
+        {
+            return this.TaskValue;
+        }
+
+        public void SetTaskValue(String taskValue)
+        {
+            this.TaskValue = taskValue;
+        }
+
+        public Boolean IsCompleted()
+        {
+            return this.Completed;
+        }
+
+        public void SetCompeted(Boolean isCompleted)
+        {
+            this.Completed = isCompleted;
+        }
+    }
+}
