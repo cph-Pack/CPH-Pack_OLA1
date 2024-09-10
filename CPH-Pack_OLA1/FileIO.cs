0abdca2a819788c4916c30b3010fa3110c08793f
Dieky <36627007+Dieky@users.noreply.github.com>
2024-09-10 08:52:23 +0200

    Can read and write to file
    FileIO class for writing to class.
    TestTask class was used for dummy data while coding. This class should be removed at some point.

---
+++ CPH-Pack_OLA1/FileIO.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CPH_Pack_OLA1
+{
+    class FileIO
+    {
+        string docPath;
+
+        public FileIO()
+        {
+            this.docPath = System.IO.Directory.GetCurrentDirectory() + "\\testfile.txt";
+        }
+
+        public List<TestTask> Read_File()
+        {
+            try
+            {
+                // Open the text file using a stream reader.
+                using StreamReader reader = new(this.docPath);
+
+                // Read the stream as a string and deserialize content to a list
+                string fileContent = reader.ReadToEnd();
+                List<TestTask> tasks = JsonSerializer.Deserialize<List<TestTask>>(fileContent);
+
+                // return the list to the caller.
+                return tasks;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return null;
+        }
+
+        public void Write_File(List<TestTask> tasks)
+        {
+
+            // prepare options, convert array to json string and write to file
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonString = JsonSerializer.Serialize(tasks, options);
+            File.WriteAllText(this.docPath, jsonString);
+
+        }
+
+        // will be deleted but this was the old method
+        public void Write_File_Old(TestTask task)
+        {
+            // read content
+            List<TestTask> tasks = Read_File();
+
+            // add new task
+            tasks.Add(task);
+
+            // prepare options, convert array to json string and write to file
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonString = JsonSerializer.Serialize(tasks, options);
+            File.WriteAllText(this.docPath, jsonString);
+
+        }
+
+
+    }
+}
---
+++ CPH-Pack_OLA1/TestTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPH_Pack_OLA1
+{
+    internal class TestTask
+    {
+        public required String TaskName { get; set; }
+        public required String TaskValue { get; set; }
+        public DateOnly Deadline { get; set; } = new DateOnly();
+        public Boolean IsCompleted { get; set; } = false;
+
+    }
+}
